a44176e7875c1decf62ee27b200c0c1d97c36ffd
Srijan Baniyal <107460791+Srijan-Baniyal@users.noreply.github.com>
2023-10-12 16:34:29 +0530

    Create Area of a Circle.js

---
+++ JavaScript/Area of a Circle.js
@@ -0,0 +1,6 @@
+const radius = 10;
+const pi = Math.PI;
+const areaOfCircle = (radius, pi) => {
+   return pi * radius * radius;
+};
+console.log("The area of the circle is: " + areaOfCircle(radius, pi));
