f89762c75fe21f798c7104b9d31c7d844af52a98
parthiv_ghose <parthivghose2919@gmail.com>
2023-10-13 01:18:05 +0530

    fixed some of the loopholes in the previous solution

--- Python/mergesort_recursion.py
+++ Python/mergesort_recursion.py
@@ -1,35 +1,33 @@
 def merge_sort(arr):
-    if len(arr) > 1:
-        # Divide the array into two halves
-        mid = len(arr) // 2
-        left_half = arr[:mid]
-        right_half = arr[mid:]
-
-        # Recursively sort both halves
-        merge_sort(left_half)
-        merge_sort(right_half)
-
-        # Merge the sorted halves
-        i = j = k = 0
-
-        while i < len(left_half) and j < len(right_half):
-            if left_half[i] < right_half[j]:
-                arr[k] = left_half[i]
+    def merge(left, right):
+        result = []
+        i = j = 0
+        while i < len(left) and j < len(right):
+            if left[i] < right[j]:
+                result.append(left[i])
                 i += 1
             else:
-                arr[k] = right_half[j]
+                result.append(right[j])
                 j += 1
-            k += 1
+        result.extend(left[i:])
+        result.extend(right[j:])
+        return result
 
-        while i < len(left_half):
-            arr[k] = left_half[i]
-            i += 1
-            k += 1
+    if len(arr) <= 1:
+        return arr
 
-        while j < len(right_half):
-            arr[k] = right_half[j]
-            j += 1
-            k += 1
+    stack = [[i] for i in arr]
+
+    while len(stack) > 1:
+        new_stack = []
+        for i in range(0, len(stack) - 1, 2):
+            merged = merge(stack[i], stack[i + 1])
+            new_stack.append(merged)
+        if len(stack) % 2 == 1:
+            new_stack.append(stack[-1])
+        stack = new_stack
+
+    return stack[0]
 
 # Take user input for the array
 input_str = input("Enter space-separated elements: ")
@@ -37,7 +35,12 @@ user_input = input_str.split()
 arr = [int(x) for x in user_input]
 
 # Call the merge_sort function
-merge_sort(arr)
+sorted_arr = merge_sort(arr)
 
 # Display the sorted array
-print("Sorted array is:", arr)
+print("Sorted array is:", sorted_arr)
+#In this implementation, the merge function is used for merging two subarrays efficiently.
+#  The main optimization is in the iterative merging process using a stack, which eliminates the need for recursive function calls. 
+# This can be more efficient in practice, especially for large arrays.
+#The previous solution consists some loopholes such that due to unnecessary recursive calls 
+# there is some extra use of memory which is avoided in this solution 
