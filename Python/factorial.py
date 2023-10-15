8f2957ab665d25115caf844ff4d7f00042a48948
MANTHAN R M <122231661+Anorak001@users.noreply.github.com>
2023-10-15 22:40:00 +0530

    Update factorial.py
    I have specified the user to enter positive number only,added few comments.
    this is my first contribution to hacktoberfest

--- Python/factorial.py
+++ Python/factorial.py
@@ -1,3 +1,5 @@
 import math
-n=int(input("Enter a number: "))
-print("Factorial of",n,"is",math.factorial(n))
+n=int(input("Enter a positive number: "))
+#Factorial is pre_loaded function in python.
+print("Factorial of",n,"=",math.factorial(n))
+print("THANK YOU")
