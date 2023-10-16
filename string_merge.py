f7e672be0262a4de4b1148d3abbf23a7e8f31f7e
Vermaji <101314398+Vermaji8620@users.noreply.github.com>
2023-10-17 01:09:25 +0530

    Create string_merge.py

---
+++ string_merge.py
@@ -0,0 +1,9 @@
+# Input two strings from the user
+first_string = input("Enter the first string: ")
+second_string = input("Enter the second string: ")
+
+# Concatenate the strings
+concatenated_string = first_string + second_string
+
+# Print the concatenated string
+print("Concatenated String:", concatenated_string)
