af2ddd39b561d873633a5eb9bdcb56f7816486f3
zedTeam21 <103349442+zedTeam21@users.noreply.github.com>
2023-10-18 09:55:28 +0530

    Update factorial.py

--- Python/factorial.py
+++ Python/factorial.py
@@ -1,3 +1,16 @@
 import math
-n=int(input("Enter a number: "))
-print("Factorial of",n,"is",math.factorial(n))
+
+# Prompt the user for input
+user_input = input("Enter a number to calculate its factorial: ")
+
+try:
+    # Convert the input to an integer
+    n = int(user_input)
+    
+    # Calculate the factorial of the input number using math.factorial
+    result = math.factorial(n)
+    
+    # Display the result
+    print(f"Factorial of {n} is {result}")
+except ValueError:
+    print("Invalid input. Please enter a valid integer.")
