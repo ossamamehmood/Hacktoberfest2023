47d8fba5e76d3453eb484477f20881d91c766e6b
Chandra Prakash S <scp.thedevil@gmail.com>
2023-10-22 14:59:54 +0530

    Added Linkedlist implementation in javascript

---
+++ JavaScript/linkedList.js
@@ -0,0 +1,68 @@
+class Node {
+  constructor(data, next = null) {
+    this.data = data;
+    this.next = next;
+  }
+}
+
+class LinkedList {
+  constructor() {
+    this.head = null;
+  }
+
+  insertFirst(data) {
+    this.head = new Node(data, this.head);
+  }
+
+  size() {
+    let counter = 0;
+    let node = this.head;
+
+    while(node) {
+      counter++;
+      node = node.next;
+    }
+
+    return counter;
+  }
+
+  getFirst() {
+    return this.head;
+  }
+
+  getLast() {
+    if (!this.head) {
+      return null;
+    }
+
+    let node = this.head;
+
+    while(node) {
+      if (!node.next) {
+        return node;
+      }
+      node = node.next;
+    }
+  }
+
+  clear() {
+    this.head = null;
+  }
+
+  removeFirst() {
+    if (!this.head) {
+      return;
+    }
+
+    this.head = this.head.next;
+  }
+}
+
+const list = new LinkedList();
+list.insertFirst('Hello');
+list.insertFirst('World');
+console.log(list.size()); // Outputs: 2
+console.log(list.getFirst().data); // Outputs: World
+console.log(list.getLast().data); // Outputs: Hello
+list.removeFirst();
+console.log(list.getFirst().data); // Outputs: Hello
