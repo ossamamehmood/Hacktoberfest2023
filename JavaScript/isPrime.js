7eb1ad2f927007d6872672ed3ea26cd28dd2b9cd
vanillae <alharitsfaqih@gmail.com>
2023-10-24 00:09:42 +0700

    refactor: Improve readability of isPrime function

--- JavaScript/isPrime.js
+++ JavaScript/isPrime.js
@@ -1,21 +1,23 @@
-// This JavaScript code checks if a given number is prime.
+// Check if number is prime
 function isPrime(number) {
-    if (number <= 1) {
-      return false;
-    }
-  
-    for (let i = 2; i <= Math.sqrt(number); i++) {
-      if (number % i === 0) {
-        return false;
-      }
-    }
-  
-    return true;
+
+  // Numbers less than 2 are not prime
+  if (number < 2) return false;
+
+  // Check divisibility from 2 to square root of number
+  for (let i = 2; i <= Math.sqrt(number); i++) {
+    if (number % i == 0) return false;
   }
-  
-  const num = 17;
-  if (isPrime(num)) {
-    console.log(`${num} is a prime number.`);
-  } else {
-    console.log(`${num} is not a prime number.`);
-  }
+
+  return true;
+
+}
+
+// Usage
+const num = 17;
+
+if (isPrime(num)) {
+  console.log(`${num} is prime`);
+} else {
+  console.log(`${num} is not prime`);
+}
