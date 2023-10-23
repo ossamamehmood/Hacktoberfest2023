423033b11116390994eed05aa417578d7b0a1bdd
Aaradhya Pathak <aaradhyapathakofficial@gmail.com>
2023-10-23 12:09:39 +0530

    added api.js for fetching api

--- JavaScript/API.js
+++ JavaScript/API.js
@@ -1,11 +1,11 @@
 const apiCall = async () => {
   const url =
-    "https://weatherapi-com.p.rapidapi.com/current.json?q=53.1%2C-0.13";
+    "https://weatherapi-com.p.rapidapi.com/${xyzabc123}";
   const options = {
     method: "GET",
     headers: {
-      "X-RapidAPI-Key": "4063153c18msh7e3ad85b895719dp1187f7jsn5e0d6c7ae266",
-      "X-RapidAPI-Host": "weatherapi-com.p.rapidapi.com",
+      "X-RapidAPI-Key": "xyz",
+      "X-RapidAPI-Host": "abc",
     },
   };
 
