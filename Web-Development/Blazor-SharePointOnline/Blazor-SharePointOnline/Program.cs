e14a5f7cdda196dc2c482cbcb2b50c2b2e0eb51b
francis92 <roshanfernando92@hotmail.com>
2023-10-24 07:51:14 +0530

    adding blazor web application using sharepoint csom

---
+++ Web-Development/Blazor-SharePointOnline/Blazor-SharePointOnline/Program.cs
@@ -0,0 +1,75 @@
+using BlazorApp2.Data;
+using BlazorApp2.Services;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Mvc.Authorization;
+using Microsoft.Graph.ExternalConnectors;
+using Microsoft.Identity.Web;
+using Microsoft.Identity.Web.UI;
+
+var builder = WebApplication.CreateBuilder(args);
+
+var initialScopes = builder.Configuration["DownstreamApi:Scopes"]?.Split(' ') ?? builder.Configuration["MicrosoftGraph:Scopes"]?.Split(' ');
+
+// Add services to the container.
+builder.Services.AddAuthentication(OpenIdConnectDefaults.AuthenticationScheme)
+    .AddMicrosoftIdentityWebApp(builder.Configuration.GetSection("AzureAd"))
+        .EnableTokenAcquisitionToCallDownstreamApi(initialScopes)
+.AddMicrosoftGraph(builder.Configuration.GetSection("MicrosoftGraph"))
+            .AddInMemoryTokenCaches().AddDistributedTokenCaches();
+
+builder.Services.AddControllersWithViews()
+    .AddMicrosoftIdentityUI();
+
+
+builder.Services.AddAuthorization(options =>
+{
+    // By default, all incoming requests will be authorized according to the default policy
+    options.FallbackPolicy = options.DefaultPolicy;
+});
+builder.Services.AddDistributedMemoryCache();
+
+builder.Services.AddSharePointContextFactory();
+
+builder.Services.AddControllersWithViews(options =>
+{
+    var policy = new AuthorizationPolicyBuilder()
+        .RequireAuthenticatedUser()
+        .Build();
+    options.Filters.Add(new AuthorizeForScopesAttribute() { Scopes = new[] { "User.Read" } });
+    options.Filters.Add(new AuthorizeFilter(policy));
+});
+builder.Services.AddRazorPages();
+builder.Services.AddServerSideBlazor()
+    .AddMicrosoftIdentityConsentHandler();
+builder.Services.AddSingleton<WeatherForecastService>();
+builder.Services.AddScoped<SharePointClientContextFactory>();
+builder.Services.AddCurrentUserSharePointClientContext();
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthentication();
+
+app.UseAuthorization();
+
+app.MapControllers();
+app.MapBlazorHub();
+app.MapFallbackToPage("/_Host");
+
+app.Run();
---
+++ Web-Development/Blazor-SharePointOnline/Blazor-SharePointOnline/Services/SharePointClientContextFactoryServiceConfiguration.cs
@@ -0,0 +1,81 @@
+﻿using Microsoft.Identity.Web;
+using Microsoft.SharePoint.Client;
+
+namespace BlazorApp2.Services
+{
+    public static class SharePointClientContextFactoryServiceConfiguration
+    {
+        public static IServiceCollection AddSharePointContextFactory(this IServiceCollection serviceCollection)
+        {
+            serviceCollection.AddScoped<ISharePointClientContextFactory, SharePointClientContextFactory>();
+            return serviceCollection;
+        }
+
+        public static IServiceCollection AddCurrentUserSharePointClientContext(this IServiceCollection serviceCollection)
+        {
+            serviceCollection.AddSharePointContextFactory();
+
+            serviceCollection.AddScoped<ClientContext>((services) =>
+            {
+                var clientContextFactory = services.GetService<ISharePointClientContextFactory>();
+                return clientContextFactory.GetClientContext();
+            });
+
+            return serviceCollection;
+        }
+    }
+
+    public interface ISharePointClientContextFactory
+    {
+        ClientContext GetClientContext(string siteUrl = null);
+    }
+
+    class SharePointClientContextFactory : ISharePointClientContextFactory
+    {
+        private readonly ITokenAcquisition _tokenAcquisition;
+        private readonly IConfiguration _configuration;
+
+        public SharePointClientContextFactory(IConfiguration configuration, ITokenAcquisition tokenAcquisition)
+        {
+            _configuration = configuration;
+            _tokenAcquisition = tokenAcquisition;
+        }
+
+        private string GetResourceUri(string siteUrl)
+        {
+            var uri = new Uri(siteUrl);
+            return $"{uri.Scheme}://{uri.DnsSafeHost}";
+        }
+
+        private string[] GetSharePointResourceScope(string siteUrl, string[] scopes = null)
+        {
+            string resourceUri = GetResourceUri(siteUrl);
+            return scopes == null
+                ? new[] { $"{resourceUri}/.default" }
+                : scopes.Select(scope => $"{resourceUri}/{scope}").ToArray();
+        }
+
+
+        private ClientContext GetClientContextInternal(string siteUrl, string[]? scopes = null)
+        {
+            siteUrl ??= _configuration.GetValue<string>("SharePoint:Url");
+            if (string.IsNullOrEmpty(siteUrl))
+                throw new Exception("The SharePoint site URL is not specified or configured");
+
+            // Acquire the access token.
+            string[] effectiveScopes = GetSharePointResourceScope(siteUrl, new string[] { "User.Read" });
+            var clientContext = new ClientContext(siteUrl);
+            clientContext.ExecutingWebRequest += (object sender, WebRequestEventArgs e) =>
+            {
+                string accessToken = _tokenAcquisition.GetAccessTokenForUserAsync(effectiveScopes).GetAwaiter().GetResult();
+                e.WebRequestExecutor.RequestHeaders.Add("Authorization", $"Bearer {accessToken}");
+            };
+            return clientContext;
+        }
+
+        public ClientContext GetClientContext(string? siteUrl = null)
+        {
+            return GetClientContextInternal(siteUrl);
+        }
+    }
+}
