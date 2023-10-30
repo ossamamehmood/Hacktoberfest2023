aeb3a36f91284b2fe5a761ebe5405d4f5a9a0bef
Madurangi Jayathissa <92798397+MadurangiJayathissa@users.noreply.github.com>
2023-10-30 12:07:12 +0530

    Butterfly Pattern in Java

---
+++ Java/ButterflyPattern.java
@@ -0,0 +1,36 @@
+// Butterfly Pattern in Java
+
+public class ButterflyPattern {
+    public static void main(String[] args) {
+        int n = 5; // Change this value to adjust the size of the butterfly pattern
+        printButterflyPattern(n);
+    }
+
+    public static void printButterflyPattern(int n) {
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("* ");
+            }
+            for (int j = 1; j <= 2 * (n - i); j++) {
+                System.out.print("  ");
+            }
+            for (int j = 1; j <= i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+
+        for (int i = n; i >= 1; i--) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("* ");
+            }
+            for (int j = 1; j <= 2 * (n - i); j++) {
+                System.out.print("  ");
+            }
+            for (int j = 1; j <= i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
